fa73aa5728b9b4abe4050fc0b3b65d5c867849c5
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-24 20:33:44 +0800

    i18n: TBL batch2 recipes (Vital/Shadowless/Ultimate Era/Alchemist/drinks)

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -13,6 +13,7 @@ merge_files = [
     root / "data" / "length_fix_delta.tsv",
     root / "data" / "ui_batch_delta.tsv",
     root / "data" / "tbl_batch_delta.tsv",
+    root / "data" / "tbl_batch2_delta.tsv",
 ]
 
 M = {}
