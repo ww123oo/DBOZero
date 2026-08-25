c35ad55bd14668e63c588cdf2f4aea888e183b3a
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-26 00:09:47 +0800

    i18n: TBL batch21 useful (Goku/Frieza, Flying=舞空, Giant Radar, keys)

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -35,6 +35,7 @@ merge_files = [
     root / "data" / "tbl_batch18_delta.tsv",
     root / "data" / "tbl_batch19_delta.tsv",
     root / "data" / "tbl_batch20_delta.tsv",
+    root / "data" / "tbl_batch21_delta.tsv",
 ]
 
 M = {}
