13a5f7fe6e14e74470d5223f376a025afccc7e3e
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-26 18:06:17 +0800

    i18n: TBL batch24 ~120 (Korin, Goku, Flying=舞空, pills, passives)

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -39,6 +39,7 @@ merge_files = [
     root / "data" / "tbl_batch21_delta.tsv",
     root / "data" / "tbl_batch22_delta.tsv",
     root / "data" / "tbl_batch23_delta.tsv",
+    root / "data" / "tbl_batch24_delta.tsv",
 ]
 
 M = {}
