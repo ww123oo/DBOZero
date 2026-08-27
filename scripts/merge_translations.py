e0a960750430803ec360651508452d60e01c1f33
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-27 17:05:29 +0800

    i18n: TBL batch45 ~100 (Ultimate skills continued)

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -23,7 +23,7 @@ merge_files = [
     root / "data" / name
     for name in [
         "tbl_batch_delta.tsv",
-        *[f"tbl_batch{i}_delta.tsv" for i in range(2, 45)],
+        *[f"tbl_batch{i}_delta.tsv" for i in range(2, 46)],
     ]
 ]
 
