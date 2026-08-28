c3af0a106f7af4f3524a76fda1e783cf726566a3
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-28 23:58:58 +0800

    i18n: tbl2 batch67 weapons masks beginner equip

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -47,7 +47,7 @@ merge_files = [
     root / "data" / "term_kaili_fix_delta.tsv",
 ] + [
     root / "data" / name
-    for name in ["tbl_batch_delta.tsv", *[f"tbl_batch{i}_delta.tsv" for i in range(2, 67)]]
+    for name in ["tbl_batch_delta.tsv", *[f"tbl_batch{i}_delta.tsv" for i in range(2, 68)]]
 ]
 
 M = {}
