e7502318ea7963f1fc738d3f8c09d6e14dfcf924
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-29 18:23:16 +0800

    i18n: tbl2 batch80-81 gloves claws axes gems ~150

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -49,7 +49,7 @@ merge_files = [
     root / "data" / "term_kaili_fix_delta.tsv",
 ] + [
     root / "data" / name
-    for name in ["tbl_batch_delta.tsv", *[f"tbl_batch{i}_delta.tsv" for i in range(2, 80)]]
+    for name in ["tbl_batch_delta.tsv", *[f"tbl_batch{i}_delta.tsv" for i in range(2, 82)]]
 ]
 
 M = {}
