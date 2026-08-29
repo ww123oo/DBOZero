c7462d301092fd03b6b8dfe8b746514fae72bc6d
ww123oo <152338335+ww123oo@users.noreply.github.com>
2026-08-29 20:48:10 +0800

    i18n: tbl2 batch93-94 packages EV hats ~150; 3K=3000現金點

--- scripts/merge_translations.py
+++ scripts/merge_translations.py
@@ -50,7 +50,7 @@ merge_files = [
     root / "data" / "term_kaili_fix_delta.tsv",
 ] + [
     root / "data" / name
-    for name in ["tbl_batch_delta.tsv", *[f"tbl_batch{i}_delta.tsv" for i in range(2, 93)]]
+    for name in ["tbl_batch_delta.tsv", *[f"tbl_batch{i}_delta.tsv" for i in range(2, 95)]]
 ]
 
 M = {}
